3af47e7ec2c1531c275029c3e097a8ff94d0a2df
ENZO FELIX CARRILHO <25132408@CFP127ALUNO.LOCAL>
2025-10-07 11:12:22 -0300

    endpoint da lista de todods os filmes no app

---
+++ app.js
@@ -0,0 +1,39 @@
+/***********************************************************************************************************
+ * Objetivo: Arquivo responsável pelas requisições da API da locadora de Filmes 
+ * Data: 07/10/2025
+ * Autor: Enzo
+ * Versão: 1.0
+ ***********************************************************************************************************/
+
+//Import das dependencias da API
+const express =    require('express')
+const cors =       require('cors') 
+const bodyParser = require('body-parser')
+
+//Import do arquivo de funções
+const dados = require('./modulo/funcoes.js')
+
+//Retorna a porta do sevidor atual ou colocamos uma porta local
+const PORT = process.PORT || 8080
+
+//Criando uma instância de uma classe do express
+const app = express()
+
+//Configuraçõs do cors
+app.use((request, response, next)=>{
+    response.header('Acces-Control-Allow-Origin', '*')
+    response.header('Acces-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+    
+    app.use(cors())
+    next() 
+})
+
+//Import das controllers
+const controller_filme = require('./controller/filme/controller_filme.js')
+
+//EndPoints para a rota de Filme
+app.get('/v1/locadora/filme', cors(), async (request, response) => {
+    //Chama a função para listar os filmes do BD
+    let filme = await controller_filme.listarFilmes()
+    response.status(filme.status_code).json(filme)
+})
