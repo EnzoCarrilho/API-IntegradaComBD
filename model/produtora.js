3fcb856f1701784eaf2cab2ed4084b7b14089c1d
EnzoCarrilho <enzocarrilho25@gmail.com>
2025-11-03 18:51:07 -0300

    implementando post de produtora camada: model

--- model/produtora.js
+++ model/produtora.js
@@ -44,7 +44,59 @@ const getSelectByIdProducer = async function(id){
     }
 }
 
+const getSelectLastId = async function(){
+    try {
+        //Script SQL que retorna apenas o último ID do BD
+        let sql = `select produtora_id from tbl_produtora order by produtora_id desc limit 1;`
+
+        let result = await prisma.$queryRawUnsafe(sql)
+
+        if(Array.isArray(result))
+            return Number(result[0].produtora_id)
+        else
+            return false
+    } catch (error) {
+        return false
+    }
+}
+
+const setInsertProducer = async function(produtora){
+
+    try {
+
+        if(produtora.descicao != undefined){
+
+            let sql = `INSERT INTO tbl_produtora(nome, ano_fundaco, logo_url, descricao)
+                            values('${produtora.nome}', '${produtora.ano_fundacao}', '${produtora.logo_url}', '${produtora.descricao}');`
+
+            let result = await prisma.$executeRawUnsafe(sql)
+            
+            if(result)
+                return result
+            else
+                return false
+
+        }else{
+
+            let sql = `INSERT INTO tbl_produtora(nome, ano_fundaco, logo_url, descricao)
+                            values('${produtora.nome}', '${produtora.ano_fundacao}', '${produtora.logo_url}', ${null} );`
+
+            let result = await prisma.$executeRawUnsafe(sql)
+            
+            if(result)
+                return result
+            else
+                return false
+        }
+
+    } catch (error) {
+        return false
+    }
+}
+
 module.exports = {
     getSelectAllProducers,
-    getSelectByIdProducer
+    getSelectByIdProducer,
+    getSelectLastId,
+    setInsertProducer
 }
