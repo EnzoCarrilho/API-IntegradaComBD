a1752612a684cf36035f8ae42e9df42cb802683c
ENZO FELIX CARRILHO <25132408@CFP127ALUNO.LOCAL>
2025-11-05 10:22:22 -0300

    criado model para relação entre filme e generos

---
+++ controller/filme/controller_filme_genero.js
@@ -0,0 +1,202 @@
+/*****************************************************************************************************************************
+ * Objetivo: Arquivo responsável pela manipulação de dados entre o APP e a MODEL para o CRUD na relação entre Filme e Gênero
+ * Data: 05/11/2025
+ * Autor: Enzo
+ * Versão: 1.0
+ *****************************************************************************************************************************/
+//Import do model da DAO do genero
+const filmeGeneroDAO = require('../../model/dao/filme_genero.js')
+
+//Import do arquivo de mensagens
+const DEFAULT_MESSAGES = require('../modulo/config_messages.js')
+
+const listarGeneros = async function() {
+    
+    //Criando um objeto novo para as mensagens
+    let MESSAGES = JSON.parse(JSON.stringify(DEFAULT_MESSAGES))
+
+    try {
+        
+        let resultGeneros = await generoDAO.getSelectAllGenres() 
+
+        if(resultGeneros){
+            if(resultGeneros.length > 0){
+                MESSAGES.DEFAULT_HEADER.status = MESSAGES.SUCCESS_REQUEST.status
+                MESSAGES.DEFAULT_HEADER.status_code = MESSAGES.SUCCESS_REQUEST.status_code
+                MESSAGES.DEFAULT_HEADER.items.generos = resultGeneros
+
+                return MESSAGES.DEFAULT_HEADER //200
+            }else{
+                return MESSAGES.ERROR_NOT_FOUND//404
+            }
+
+        }else{
+            return MESSAGES.ERROR_INTERNAL_SERVER_MODEL //500
+        }
+
+    } catch (error) {
+        return MESSAGES.ERROR_INTERNAL_SERVER_CONTROLLER //500
+    }
+
+}
+
+const buscarGeneroID = async function(id){
+    //Criando um objeto novo para as mensagens
+    let MESSAGES = JSON.parse(JSON.stringify(DEFAULT_MESSAGES))
+
+    try {
+        //Validando a chegada do ID
+        if(!isNaN(id) && id != '' && id > 0){
+            let resultGenero = await generoDAO.getSelectByIdGenres(Number(id))
+
+            if(resultGenero){
+
+                if(resultGenero.length > 0){
+                    MESSAGES.DEFAULT_HEADER.status = MESSAGES.SUCCESS_REQUEST.status
+                    MESSAGES.DEFAULT_HEADER.status_code = MESSAGES.SUCCESS_REQUEST.status_code
+                    MESSAGES.DEFAULT_HEADER.items.genero = resultGenero
+
+                    return MESSAGES.DEFAULT_HEADER //200
+
+                }else{
+                    return MESSAGES.ERROR_NOT_FOUND //404
+                }
+
+            }else{
+                return MESSAGES.ERROR_INTERNAL_SERVER_MODEL //500
+            }
+
+        }else{
+            MESSAGES.ERROR_REQUIRED_FIELDS.message += ' [ID Incorreto]'
+            return MESSAGES.ERROR_REQUIRED_FIELDS //400
+        }
+    } catch (error) {
+        return MESSAGES.ERROR_INTERNAL_SERVER_CONTROLLER //500
+    }
+}
+
+const inserirGenero = async function(genero, contentType){
+
+    let MESSAGES = JSON.parse(JSON.stringify(DEFAULT_MESSAGES))
+
+    if(String(contentType).toUpperCase() == 'APPLICATION/JSON'){
+
+        //Validação da entrada de dados
+        if(genero.nome == '' || genero.nome == undefined || genero.nome == null || genero.nome.length > 100){
+            MESSAGES.ERROR_REQUIRED_FIELDS.message += ' [Nome de Gênero Inválido]' 
+
+            return MESSAGES.ERROR_REQUIRED_FIELDS //400
+        }else{
+
+            let resultGeneros = await generoDAO.setInsertGenre(genero)
+
+            //Adicionar Filme no retorno
+            if(resultGeneros){
+
+                let lastId = await generoDAO.getSelectLastId()
+                
+                if(lastId){
+                    genero.id = lastId
+
+                    MESSAGES.DEFAULT_HEADER.status = MESSAGES.SUCCESS_CREATED_ITEM.status
+                    MESSAGES.DEFAULT_HEADER.status_code = MESSAGES.SUCCESS_CREATED_ITEM.status_code
+                    MESSAGES.DEFAULT_HEADER.message = MESSAGES.SUCCESS_CREATED_ITEM.message
+                    MESSAGES.DEFAULT_HEADER.items = genero
+                }else{
+                    MESSAGES.ERROR_INTERNAL_SERVER_MODEL
+                }            
+                return MESSAGES.DEFAULT_HEADER //201
+            }else{
+                return MESSAGES.ERROR_INTERNAL_SERVER_MODEL
+            }
+        }
+
+    }else{
+        return MESSAGES.ERROR_CONTENT_TYPE //415
+    }
+
+}
+
+const atualizarGenero = async function (genero, id, contentType){
+    
+    let MESSAGES = JSON.parse(JSON.stringify(DEFAULT_MESSAGES))
+
+    if(String(contentType).toUpperCase() == 'APPLICATION/JSON'){
+
+        //Validação da entrada de dados
+        if(genero.nome == '' || genero.nome == undefined || genero.nome == null || genero.nome.length > 100){
+            MESSAGES.ERROR_REQUIRED_FIELDS.message += ' [Nome de Gênero Inválido]' 
+
+            return MESSAGES.ERROR_REQUIRED_FIELDS //400
+        }else{
+            let validarID = await buscarGeneroID(id)
+
+            if(validarID.status_code == 200){
+                genero.id = Number(id)
+
+                let resultGeneros = await generoDAO.setUpdateGenres(genero)
+
+                if(resultGeneros){
+                    MESSAGES.DEFAULT_HEADER.status = MESSAGES.SUCCESS_UPDATED_ITEM.status
+                    MESSAGES.DEFAULT_HEADER.status_code = MESSAGES.SUCCESS_UPDATED_ITEM.status_code
+                    MESSAGES.DEFAULT_HEADER.message = MESSAGES.SUCCESS_UPDATED_ITEM.message
+                    MESSAGES.DEFAULT_HEADER.items.genero = genero
+                    
+                    return MESSAGES.DEFAULT_HEADER //201
+                }else{
+                    return MESSAGES.ERROR_INTERNAL_SERVER_MODEL
+                }
+            }else{
+                return validarID
+            }
+        }
+    }else{
+        return MESSAGES.ERROR_CONTENT_TYPE
+    }        
+
+}
+
+const excluirGenero = async function(id){
+
+    let MESSAGES = JSON.parse(JSON.stringify(DEFAULT_MESSAGES))
+    
+    try {
+            
+        let validarId = await buscarGeneroID(id)
+            
+        if(validarId.status_code == 200){
+                
+            let resultGeneros = await generoDAO.setDeleteGenres(Number(id))
+            
+    
+            if(resultGeneros){
+                MESSAGES.DEFAULT_HEADER.status = MESSAGES.SUCCESS_DELETED_ITEM.status
+                MESSAGES.DEFAULT_HEADER.status_code = MESSAGES.SUCCESS_DELETED_ITEM.status_code
+                MESSAGES.DEFAULT_HEADER.message = MESSAGES.SUCCESS_DELETED_ITEM.message
+                delete MESSAGES.DEFAULT_HEADER.items
+                    
+                return MESSAGES.DEFAULT_HEADER //200
+            }else{
+                return MESSAGES.ERROR_NOT_FOUND //404
+                }
+    
+            }else{
+                return validarId
+            }
+    
+        } catch (error) {
+            return MESSAGES.ERROR_INTERNAL_SERVER_CONTROLLER //500
+        }
+        
+    
+}
+
+
+
+module.exports = {
+    listarGeneros,
+    buscarGeneroID,
+    inserirGenero,
+    atualizarGenero,
+    excluirGenero
+}
---
+++ model/dao/filme_genero.js
@@ -0,0 +1,178 @@
+/***********************************************************************************************************
+ * Objetivo: Arquivo responsável pelo CRUD de dados no MySQL referente ao relacionamento de filme e gêneros
+ * Data: 05/10/2025
+ * Autor: Enzo
+ * Versão: 1.0
+ ***********************************************************************************************************/
+
+
+//Import da dependência do Prisma que permite a execução de Script SQL no BD
+const { PrismaClient } = require('../../generated/prisma')
+
+//Cria um novo objeto baseado na classe do PrismaClient
+const prisma = new PrismaClient()
+
+//Retorna todos os filmes e gêneros cadastrados no BD
+const getSelectAllMoviesGenres = async function(){
+    try {
+
+        let sql = `select * from tbl_filme_genero order by filme_genero_id desc;`
+
+        let result = await prisma.$queryRawUnsafe(sql)
+        
+        if(Array.isArray(result))
+            return result
+        else
+            return false
+
+    } catch (error) {
+        return false
+    }
+}
+
+//Retorna um Gênero Filtrando pelo ID
+const getSelectByIdMoviesGenres = async function(id){
+    try {
+        let sql = `select * from tbl_filme_genero where id = ${id};`
+
+        let result = await prisma.$queryRawUnsafe(sql)
+
+        if(Array.isArray(result))
+            return result
+        else
+            return false
+
+    } catch (error) {
+        return false
+    }
+}
+
+//Retorna uma lista de Gêneros filtrando pelo ID do filme
+const getSelectGenresByIdMovies = async function(idFilme){
+    try {
+        let sql = `select tbl_genero.id, tbl_genero.nome
+                        from tbl_filme
+                            inner join tbl_filme_genero
+                                on tbl_filme.id = tbl_filme_genero.filme.id
+                            inner join tbl_genero 
+                                on tbl_genero.genero_id = tbl_filme_genero.genero_id 
+                        where tbl_filme.id = ${idFilme};`
+
+        let result = await prisma.$queryRawUnsafe(sql)
+
+        if(Array.isArray(result))
+            return result
+        else
+            return false
+
+    } catch (error) {
+        return false
+    }
+}
+
+//Retorna uma lista de Filmes filtando pelo gênero
+const getSelectMoviesByIdGenre = async function(idGenero){
+    try {
+        let sql = `select tbl_filme.id, tbl_filme.nome
+                        from tbl_filme
+                            inner join tbl_filme_genero
+                                on tbl_filme.id = tbl_filme_genero.filme.id
+                            inner join tbl_genero 
+                                on tbl_genero.genero_id = tbl_filme_genero.genero_id 
+                        where tbl_genero.id = ${idGenero};`
+
+        let result = await prisma.$queryRawUnsafe(sql)
+
+        if(Array.isArray(result))
+            return result
+        else
+            return false
+
+    } catch (error) {
+        return false
+    }
+}
+
+//Retorna o último ID do Gênero Cadastrado
+const getSelectLastId = async function(){
+    try {
+        //Script SQL que retorna apenas o último ID do BD
+        let sql = `select id from tbl_filme_genero order by id desc limit 1;`
+
+        let result = await prisma.$queryRawUnsafe(sql)
+
+        if(Array.isArray(result))
+            return Number(result[0].genero_id)
+        else
+            return false
+    } catch (error) {
+        return false
+    }
+}
+
+const setInsertMoviesGenre = async function(filmeGenero){
+    try {
+
+        let sql = `INSERT INTO tbl_filme_genero (filme_id, genero_id)
+                    values( ${filmeGenero.filme_id}, ${filmeGenero.genero_id} );`
+
+        let result = await prisma.$executeRawUnsafe(sql)
+        
+        if(result)
+            return result
+        else
+            return false
+
+    } catch (error) {
+        return false
+    }
+}
+
+const setUpdateMoviesGenres = async function(filmeGenero){
+    try {
+        let sql = `UPDATE tbl_filme_genero SET 
+                    filme_id = ${filmeGenero.filme_id} 
+                    genero_id = ${filmeGenero.genero_id} 
+                    WHERE id = ${filmeGenero.id};`
+
+        let result = await prisma.$executeRawUnsafe(sql)
+
+        if(result)
+            return result
+        else
+            return false
+
+    } catch (error) {
+        return false
+    }
+        
+}
+
+const setDeleteMoviesGenres = async function(id){
+    try {
+        
+        let sql = `delete from tbl_filme_genero where id = ${id}`
+
+        let result = await prisma.$executeRawUnsafe(sql)
+        
+
+        if(result)
+            return result
+        else
+            return false
+
+    } catch (error) {
+        return false
+    }
+}
+
+module.exports = {
+    getSelectAllMoviesGenres,
+    getSelectByIdMoviesGenres,
+    getSelectGenresByIdMovies,
+    getSelectMoviesByIdGenre,
+    getSelectLastId,
+    setInsertMoviesGenre,
+    setUpdateMoviesGenres,
+    setDeleteMoviesGenres
+}
